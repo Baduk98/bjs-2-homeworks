fc7af6af5a52ed1fe8a822011d5d01a35633d7cc
Valery <vobezdchikov122@gmail.com>
2025-01-24 13:05:26 +0300

    Задания. Массивы. Готово.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
-function compareArrays(arr1, arr2) {
+function compareArrays(arr1, arr2) { 
   
-}
+    if (arr1.length !== arr2.length) { 
+      return false; 
+    } 
+    
+    return arr1.every((element, index) => element === arr2[index]); 
+  }
+  
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+  function getUsersNamesInAgeRange(users, gender) {
+
+    const filteredGender = users.filter((user) => user.gender === gender);
+    if (filteredGender.length === 0) {
+        return 0;
+    }
+
+    const ageSum = filteredGender.reduce((sum, user) => sum + user.age, 0);
+    return ageSum / filteredGender.length;
 }
